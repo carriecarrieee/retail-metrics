8277bf5e6d3244f0d2e2ccb8985a35246be38f4f
Carrie Li <carrie.t.li@gmail.com>
2018-09-27 18:37:57 +0000

    Wrapped program into a class and fixed get_df() to download data only once

--- metrics.py
+++ metrics.py
@@ -5,198 +5,225 @@ import numpy as np
 import pandas as pd
 
 
-def create_df():
-    """Creates a Panda DataFrame with transaction data."""
+class Metrics:
 
-    # Trips to the store for various energy drink brands in CSV format.
-    # Each line belongs to a purchase at a retailer for a given parent brand,
-    # including total dollars for the item.
-    url = "https://s3.amazonaws.com/isc-isc/trips_gdrive.csv"
-
-    # Test data of first 100 rows: 
-    #url = "data/transactions.head.csv"
-
-    try:
-        data = pd.read_csv(url, parse_dates=['Date'], infer_datetime_format=True)
-        df = pd.DataFrame(data) # Create DataFrame from CSV file
-    except:
-        print "Error: No data found!"
-    else:
-        return df
+    def __init__(self):
+        self.df = None
 
 
-################################################################################
+    def get_df(self):
+        """Creates a Panda DataFrame with transaction data."""
+           
+        if not self.df:
+            # Trips to the store for various energy drink brands in CSV format.
+            # Each line belongs to a purchase at a retailer for a given parent 
+            # brand, including total dollars for the item.
+            
+            # Test data of first 100 rows: 
+            url = "data/transactions.head.csv"
 
+            # url = "https://s3.amazonaws.com/isc-isc/trips_gdrive.csv"
 
-def retailer_affinity(focus_brand):
-    """Returns the strongest retailer affinity of focus brand relative to other 
-       brands. We will define retailer affinity by the retailer that sells the
-       highest percentage of the focus brand out of total units sold by that
-       retailer."""
-    
-    df = create_df()
-
-    # Add column of total drink units per retailer to perform math 
-    df['Total by Ret'] = df.groupby(['Retailer'])['Item Units'].transform('sum')
-
-    # Sum up column of total drink units per brand
-    df = df.groupby(['Retailer', 'Parent Brand', 'Total by Ret']) \
-        ['Item Units'].sum().reset_index()
-
-    # Add column of drinks per brand divided by total units per retailer
-    df['Percentage %'] = df['Item Units'] / df['Total by Ret'] * 100
-
-    # Create and query multiindex based on two columns in new df
-    df = df.set_index(['Retailer', 'Parent Brand']).xs(focus_brand, level='Parent Brand')
-
-    # Print row(s) with highest percentage, showing strongest retailer affinity
-    print "\n" + focus_brand + ":\n"
-    print df[df['Percentage %'] == df['Percentage %'].max()]
-    print "\n"
-
-    raw_input("Press 'Enter' to see all the retailers for " + focus_brand + ".\n\n")
-
-    print df.sort_values(by='Percentage %', ascending=False)
-    print "\n\n"
-
-
-################################################################################
-
-
-def count_hhs(brand=None, retailer=None, start_date=None, end_date=None):
-    """Returns the number of households given any of the optional inputs."""
-    
-    # Prepare df for date manipulation; 'Date' column is now the index
-    df = create_df().set_index(['Date']).sort_index()
-
-    columns = []
-    params = []
-
-    # Considering if each input was given
-    if brand:
-        columns.append('Parent Brand')
-        params.append(brand)
-
-    if retailer:
-        columns.append('Retailer')
-        params.append(retailer)
-
-    if start_date:
-        start_date = pd.to_datetime(start_date, infer_datetime_format=True)
-        df = df.loc[start_date:]
-
-    if end_date:
-        end_date = pd.to_datetime(end_date, infer_datetime_format=True)
-        df = df.loc[:end_date]
-
-
-    if not columns or not params: # Case for empty lists
-        newdf = df
-
-    else:
-        if len(params) < 2: # Since tuples w/ one item have a trailing comma
-            args = params[0]
+            try:
+                self.data = pd.read_csv(url, parse_dates=['Date'], infer_datetime_format=True)
+                self.df = pd.DataFrame(self.data) # Create DataFrame from CSV file
+                return self.df
+            except:
+                print "Error: No data found!"
         else:
-            args = tuple(params) # Convert to tuple to pass into get_group()
+            return self.df
 
 
-        df = df.groupby(columns) # Group by current list of parameters
-        newdf = df['Retailer','Parent Brand','User ID'].get_group(args)
-
-    # Count number of unique User IDs from the final newdf
-    hhs = newdf['User ID'].nunique()
-
-    print "\nGiven the parameter(s), the number of unique household(s) is: {}.".format(hhs)
-    raw_input("\n\nPress 'Enter' to see the expanded results:\n\n")
-
-    print newdf
-    print "\n\n"
+    ############################################################################
 
 
-################################################################################
+    def retailer_affinity(self, focus_brand):
+        """Returns the strongest retailer affinity of focus brand relative to other 
+           brands. We will define retailer affinity by the retailer that sells the
+           highest percentage of the focus brand out of total units sold by that
+           retailer.
+
+           Test:
+
+           retailer_affinity('Monster')
 
 
-def top_buying_brand():
-    """Identifies the brand with the top buying rate ($ spent / HH)."""
-    
-    df = create_df()
-
-    # Convert string obj to int type
-    df['Item Dollars'] = df['Item Dollars'].str[1:].astype(int)
-
-    # Sum up $ spent by each unique household ID
-    df = df.groupby(['Parent Brand','User ID'])['Item Dollars'] \
-        .sum() \
-        .reset_index() \
-        .set_index(['Parent Brand'])
-
-    print "\n" + "Brand with the top buying rate ($ spent / HH):" + "\n"
-    print df[df['Item Dollars'] == df['Item Dollars'].max()]
-    print "\n\n"
+           retailer_affinity('Red Bull')
 
 
-################################################################################
+           retailer_affinity('Rockstar')
 
 
-def take_input():
-    """Takes user input and calls the appropriate function."""
+           retailer_affinity('5 Hour Energy')
 
-    input = raw_input("\nWhich metric would you like to see?\n\n \
-        a) Retailer Affinity \n \
-        b) Number of Households \n \
-        c) Top Buying Brand\n")
 
-    if input == 'a':
-        brand = raw_input("\nPlease enter a brand: \
-            5 Hour Energy \n \
-            Monster \n \
-            Red Bull \n \
-            Rockstar\n")
+           """
+        
+        df = Metrics().get_df()
 
-        retailer_affinity(brand)
+        # Add column of total drink units per retailer to perform math 
+        df['Total by Ret'] = df.groupby(['Retailer'])['Item Units'].transform('sum')
 
-    elif input == 'b':
-        ans1 = raw_input("\nWould you like to select an energy drink brand y/n: \n")
-        if ans1 == 'Y':
-            brand = raw_input("Enter an energy drink brand: \n \
+        # Sum up column of total drink units per brand
+        df = df.groupby(['Retailer', 'Parent Brand', 'Total by Ret']) \
+            ['Item Units'].sum().reset_index()
+
+        # Add column of drinks per brand divided by total units per retailer
+        df['Percentage %'] = df['Item Units'] / df['Total by Ret'] * 100
+
+        # Create and query multiindex based on two columns in new df
+        df = df.set_index(['Retailer', 'Parent Brand']).xs(focus_brand, level='Parent Brand')
+
+        # Print row(s) with highest percentage, showing strongest retailer affinity
+        print "\n" + focus_brand + ":\n"
+        print df[df['Percentage %'] == df['Percentage %'].max()]
+        print "\n"
+
+        raw_input("Press 'Enter' to see all the retailers for " + focus_brand + ".\n\n")
+
+        print df.sort_values(by='Percentage %', ascending=False)
+        print "\n\n"
+
+
+    ############################################################################
+
+
+    def count_hhs(self, brand=None, retailer=None, start_date=None, end_date=None):
+        """Returns the number of households given any of the optional inputs."""
+        
+        # Prepare df for date manipulation; 'Date' column is now the index
+        df = Metrics().get_df().set_index(['Date']).sort_index()
+
+        columns = []
+        params = []
+
+        # Considering if each input was given
+        if brand:
+            columns.append('Parent Brand')
+            params.append(brand)
+
+        if retailer:
+            columns.append('Retailer')
+            params.append(retailer)
+
+        if start_date:
+            start_date = pd.to_datetime(start_date, infer_datetime_format=True)
+            df = df.loc[start_date:]
+
+        if end_date:
+            end_date = pd.to_datetime(end_date, infer_datetime_format=True)
+            df = df.loc[:end_date]
+
+
+        if not columns or not params: # Case for empty lists
+            newdf = df
+
+        else:
+            if len(params) < 2: # Since tuples w/ one item have a trailing comma
+                args = params[0]
+            else:
+                args = tuple(params) # Convert to tuple to pass into get_group()
+
+
+            df = df.groupby(columns) # Group by current list of parameters
+            newdf = df['Retailer','Parent Brand','User ID'].get_group(args)
+
+        # Count number of unique User IDs from the final newdf
+        return newdf['User ID'].nunique()
+
+
+    ############################################################################
+
+
+    def top_buying_brand(self):
+        """Identifies the brand with the top buying rate ($ spent / HH).
+           Test:
+
+           top_buying_brand()
+
+        """
+        
+        df = Metrics().get_df()
+
+        # Convert string obj to int type
+        df['Item Dollars'] = df['Item Dollars'].str[1:].astype(int)
+
+        # Sum up $ spent by each unique household ID
+        df = df.groupby(['Parent Brand','User ID'])['Item Dollars'] \
+            .sum() \
+            .reset_index() \
+            .set_index(['Parent Brand'])
+
+        # Brand with the top buying rate ($ spent / HH):"
+        return df[df['Item Dollars'] == df['Item Dollars'].max()]
+
+
+    ############################################################################
+
+
+    def main(self):
+        """Takes user input and calls the appropriate function."""
+
+        input = raw_input("\nWhich metric would you like to see?\n\n \
+            a) Retailer Affinity \n \
+            b) Number of Households \n \
+            c) Top Buying Brand\n")
+
+        if input == 'a':
+            brand = raw_input("\nPlease enter a brand: \
                 5 Hour Energy \n \
                 Monster \n \
                 Red Bull \n \
                 Rockstar\n")
+
+            Metrics().retailer_affinity(brand)
+
+        elif input == 'b':
+            ans1 = raw_input("\nWould you like to select an energy drink brand y/n: \n")
+            if ans1 == 'Y':
+                brand = raw_input("Enter an energy drink brand: \n \
+                    5 Hour Energy \n \
+                    Monster \n \
+                    Red Bull \n \
+                    Rockstar\n")
+            else:
+                brand = None
+
+            ans2 = raw_input("\nWould you like to enter a retailer? y/n: \n")
+            if ans2 == 'y':
+                retailer = raw_input("Enter a retailer: \n \
+                    Costco \n \
+                    CVS \n \
+                    Kroger \n \
+                    Publix \n \
+                    Target \n \
+                    Walgreens \n \
+                    Walmart \n")
+            else:
+                brand = None
+
+            ans3 = raw_input("\nWould you like to enter a start date? y/n: \n")
+            if ans3 == 'y':
+                start_date = raw_input("Enter a start date (YYYY-MM-DD): \n")
+            else:
+                start_date = None
+
+            ans4 = raw_input("\nWould you like to enter an end date? y/n: \n")
+            if ans4 == 'y':
+                end_date = raw_input("Enter an end date (YYYY-MM-DD): \n")
+            else:
+                end_date = None
+
+            Metrics().count_hhs(brand=brand, retailer=retailer, start_date=start_date, end_date=end_date)
+
+        elif input == 'c':
+            Metrics().top_buying_brand()
+
         else:
-            brand = None
+            raise Exception("Invalid input. Please try again and enter only A, B, or C.\n")
 
-        ans2 = raw_input("\nWould you like to enter a retailer? y/n: \n")
-        if ans2 == 'y':
-            retailer = raw_input("Enter a retailer: \n \
-                Costco \n \
-                CVS \n \
-                Kroger \n \
-                Publix \n \
-                Target \n \
-                Walgreens \n \
-                Walmart \n")
-        else:
-            brand = None
 
-        ans3 = raw_input("\nWould you like to enter a start date? y/n: \n")
-        if ans3 == 'y':
-            start_date = raw_input("Enter a start date (YYYY-MM-DD): \n")
-        else:
-            start_date = None
+#if __name__ == "__main__":
+myMetrics = Metrics()
+myMetrics.main()
 
-        ans4 = raw_input("\nWould you like to enter an end date? y/n: \n")
-        if ans4 == 'y':
-            end_date = raw_input("Enter an end date (YYYY-MM-DD): \n")
-        else:
-            end_date = None
-
-        count_hhs(brand=brand, retailer=retailer, start_date=start_date, end_date=end_date)
-
-    elif input == 'c':
-        top_buying_brand()
-
-    else:
-        raise Exception("Invalid input. Please try again and enter only A, B, or C.\n")
-
-take_input()
